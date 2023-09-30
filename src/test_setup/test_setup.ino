4f7a824052668cd4ae999266509b3679ab0f5dab
HubotChen <98792428+HubotChen@users.noreply.github.com>
2023-09-29 21:33:00 -0400

    Create test_setup.ino

---
+++ src/test_setup/test_setup.ino
@@ -0,0 +1,29 @@
+#include <FastLED.h>
+#define NUM_LEDS      30
+#define LED_PIN       2
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(255);
+  Serial.begin(9600);
+}
+
+int rainbowHue = 0;
+int sat = 255;
+int bright = 255;
+
+void loop() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+  leds[i] = CHSV(rainbowHue + (i * 2), sat, bright);
+  }
+  EVERY_N_MILLISECONDS(2) {
+    rainbowHue++;
+  }
+  if (rainbowHue > 255) {
+    rainbowHue = rainbowHue - 255;
+  }
+  FastLED.show();
+
+}
